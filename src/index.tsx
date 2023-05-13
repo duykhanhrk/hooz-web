fd9ed7d2541d22b29c3771bdcce5020256f70726
duykhanhrk <tranduykhanh.rk@gmail.com>
2023-05-14 03:39:48 +0700

    feat: all

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
+import "react-activity/dist/library.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import store from './redux/store';
+import {BrowserRouter} from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from 'react-query'
+import {ThemeProvider} from 'styled-components';
+import {NotificationsProvider} from 'reapop';
+import {AppNotificationsSystem} from '@components';
+import ColorScheme from './constants/ColorScheme';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <NotificationsProvider>
+          <BrowserRouter>
+            <ThemeProvider theme={ColorScheme.LightTheme}>
+              <AppNotificationsSystem />
+              <App />
+            </ThemeProvider>
+          </BrowserRouter>
+        </NotificationsProvider>
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
 
